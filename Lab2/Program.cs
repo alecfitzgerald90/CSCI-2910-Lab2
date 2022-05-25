493bf78959ba1b8a3679c0d915fc4dfbe2cf9633
Alec Fitzgerald <alecfitzgerald90@gmail.com>
2022-05-25 12:19:25 -0400

    Add project files.

---
+++ Lab2/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+Console.WriteLine("\n\n*****Lab2*****\n\n");
+
+int choice = 0;
+do
+{
+    MainMenu();
+    Console.Write("Enter in your choice here: ");
+    choice = Convert.ToInt32(Console.ReadLine());
+    MenuOption(choice);
+} while (choice != 5);
+
+Console.Clear();
+Console.WriteLine("Thanks for using my program");
+
+
+static void MainMenu()
+{
+    Console.WriteLine("----- Menu -----");
+    Console.WriteLine("1. Add two numbers");
+    Console.WriteLine("2. Multiplication table");
+    Console.WriteLine("3. Memory size of data types");
+    Console.WriteLine("4. Five-function calculator");
+    Console.WriteLine("5. Exit");
+}
+
+
+static void MenuOption(int option)
+{
+    switch (option)
+    {
+        case 1: break;
+        case 2: break;
+        case 3: break;
+        case 4: break;
+            default: Console.WriteLine("\n\nInvalid menu choice. Try again\n\n");
+            break;
+    }
+}
